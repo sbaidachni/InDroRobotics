ddae7f205e6a2b3c800c5eebc070b7b91d48fb64
sbaidachni <sbaydach@microsoft.com>
2017-01-23 13:45:20 -0800

    some code was added

--- InDro/ImageToCloudService/Program.cs
+++ InDro/ImageToCloudService/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace ImageToCloudService
 {
@@ -10,6 +11,25 @@ namespace ImageToCloudService
     {
         static void Main(string[] args)
         {
+            if (args.Length!=1)
+            {
+                Console.WriteLine("You have to pass a directory as a parameter");
+                return;
+            }
+
+            string path = args[0];
+
+
+            if (!Directory.Exists(path))
+            {
+                Console.WriteLine("Directory doesn't exist");
+                return;
+            }
+
+            while (true)
+            {
+                
+            }
         }
     }
 }
