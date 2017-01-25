2833f520ce899eba7e8e1ba10522386cf4994262
Shaun Luttin <admin@shaunluttin.com>
2017-01-24 17:55:04 -0800

    Add test.cmd

---
+++ src/server/functions/test.cmd
@@ -0,0 +1,1 @@
+func run RecognizeImage -c "{'DeviceName':'drone1','BlobURI':'https://indrostorage.blob.core.windows.net/images/drone1_2017_1_24_13_43_24.png','Latitude':48.877199999999995,'Longitude':-123.4228}"
