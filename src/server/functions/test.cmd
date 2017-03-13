3982bb7d2242f42d3a0fba4ce0950ccdc6705071
Sergey <sbaidachni@gmail.com>
2017-03-13 14:01:11 -0700

    test file changes

--- src/server/functions/test.cmd
+++ src/server/functions/test.cmd
@@ -1,1 +1,1 @@
-func run RecognizeImage -c "{'DeviceName':'droneOne','BlobURI':'https://indrostorage.blob.core.windows.net/images/drone1_2017_1_24_13_43_24.png','Latitude':48.877199999999995,'Longitude':-123.4228}"
+func run RecognizeImage -c "{'DeviceName':'droneOne','BlobURI':'https://indrobuildstorage.blob.core.windows.net/images/droneOne_2017_3_13_13_34_13.JPG','Latitude':48.877199999999995,'Longitude':-123.4228}"
